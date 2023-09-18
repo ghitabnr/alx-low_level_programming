acccca422a1dda63086588a79a9d9ed9c80b780a
Ghita Bounouara <ghita.bnr2003@gmail.com>
2023-09-18 04:57:42 -0700

    qqqa

--- 0x17-doubly_linked_lists/4-free_dlistint.c
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -1,19 +1,20 @@
 #include "lists.h"
+#include <stdlib.h>
 
 /**
- * free_dlistint - frees a dlist
- * @head: pointer to current head node
+ * free_dlistint - free a dlistint_t list
+ * @head: pointer to the beginning of the linked list
  *
  * Return: void
  */
 void free_dlistint(dlistint_t *head)
 {
-	dlistint_t *node;
+	dlistint_t *next;
 
-	while (head)
+	while (head != NULL)
 	{
-		node = head;
-		head = head->next;
-		free(node);
+		next = head->next;
+		free(head);
+		head = next;
 	}
 }
